b518990ce9e72e6d98a8ddaecbe6e5edeead1121
Aman Dhiman vishwakarma <adhiman8439@gmail.com>
2024-05-08 10:26:33 +0530

    ui design completed

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/home/HomePage'
+import Login from './pages/auth/Login'
+import SignUp from './pages/auth/SignUp'
+import Sidebar from './components/common/Sidebar'
+import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>aman</div>
-        
+     <div className='flex max-w-6xl mx-auto'>
+      {/*This Is a Common Components*/}
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+     </div>
     </>
   )
 }
